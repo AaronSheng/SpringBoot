77bd4789c7340261ac39e898f12dadf631756b04
Aaron@ShengMBP <shengmac@126.com>
2017-08-28 23:48:10 +0800

    更新mapper

--- src/main/java/com/aaron/mapper/dynamic/DynamicMapper.java
+++ src/main/java/com/aaron/mapper/dynamic/DynamicMapper.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.Select;
 /**
  * Created by Aaron Sheng on 2017/8/27.
  */
-@Mapper
 public interface DynamicMapper {
     @Select("select * from user where id= #{id}")
     User get(@Param("id") Long id);
--- src/main/java/com/aaron/mapper/serv/UserMapper.java
+++ src/main/java/com/aaron/mapper/serv/UserMapper.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.Update;
 /**
  * Created by aaron on 7/11/17.
  */
-@Mapper
 public interface UserMapper {
     @Select("select * from user where id= #{id}")
     User get(@Param("id") Long id);
