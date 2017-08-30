1c5fb03c8b4d51137809e33d6e047a0dc8f731ea
aaronxsheng <aaronsheng91@126.com>
2017-08-30 13:41:44 +0800

    更新UserServiceImpl

--- src/main/java/com/aaron/service/UserServiceImpl.java
+++ src/main/java/com/aaron/service/UserServiceImpl.java
@@ -1,16 +1,17 @@
 package com.aaron.service;
 
 import com.aaron.dao.UserDao;
-import com.aaron.mapper_serv.UserMapper;
-import com.aaron.mapper_comm.CommunityUserMapper;
+import com.aaron.mapper.serv.UserMapper;
+import com.aaron.mapper.comm.CommunityUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Aaron Sheng on 9/9/16.
  */
 @Service
-//@Transactional(value="serverTransactionManager")
+@Transactional(value="serverTransactionManager")
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
