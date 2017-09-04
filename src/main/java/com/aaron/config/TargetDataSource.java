ec6633cd217000c7a67ca84e424fec79e8b33864
Aaron@ShengMBP <shengmac@126.com>
2017-09-04 23:13:13 +0800

    更新TargetSource注解

--- src/main/java/com/aaron/config/TargetDataSource.java
+++ src/main/java/com/aaron/config/TargetDataSource.java
@@ -8,6 +8,7 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Inherited
 public @interface TargetDataSource {
     String value();
 }
